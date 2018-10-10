3029891b19faf074bb76d9781143be2927b8f6af
Antoine Jessen <antoinejessen@hotmail.com>
2018-10-10 23:09:14 +0200

    bot twitter

---
+++ exo_1.rb
@@ -0,0 +1,12 @@
+require 'dotenv'
+require 'twitter'
+
+Dotenv.load
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["TWITTER_API_KEY"]
+  config.consumer_secret     = ENV["TWITTER_API_SECRET"]
+  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+end
+client.update('Bonjour monde !')
